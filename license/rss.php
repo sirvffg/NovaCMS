73026a430f3a9ca24e21e6390403c7b6683ef696
sirvffg <131525733+sirvffg@users.noreply.github.com>
2026-08-22 11:00:51 +0800

    chore: 删除项目中冗余的字体文件和废弃代码
    本次提交清理了以下内容：
    1. 移除了所有Font Awesome字体文件以及自定义字体文件
    2. 删除了rss.php和sitemap.php重定向脚本
    3. 删掉了多个不再使用的博客页面CSS样式文件

--- license/rss.php
+++
@@ -1,4 +0,0 @@
-<?php
-// 已迁移为 RSS 插件，重定向到新地址
-header('Location: /rss.xml', true, 301);
-exit;
--- license/sitemap.php
+++
@@ -1,4 +0,0 @@
-<?php
-// 已迁移为 Sitemap 插件，重定向到新地址
-header('Location: /sitemap.xml', true, 301);
-exit;
